fd641775232c2af7265ab736ff2058ffdb47a267
Sergio Conde Gomez <sconde@carto.com>
2019-06-11 09:49:16 +0200

    Fix improve styler script a bit.

--- mapboxgl/styler.py
+++ mapboxgl/styler.py
@@ -1,55 +1,69 @@
 '''
+This script generates Mapbox-GL style JSON files from common 'master style'
+template, and set of variables, set in separate style-specfic json file.
 
-The script generates Mapbox-GL style JSON files from common 'master style' template, and set of variables, set in 
-separate style-specfic json file.
-
-The template has to be created manually, taking a working style (Voyager) from Maputnik project and replacing 
-dynamic values by @var@ placeholders.
+The template has to be created manually, taking a working style (Voyager)
+from Maputnik project and replacing dynamic values by @var@ placeholders.
 
 The generated result files can then be used in mapbox-gl maps. Usage sample:
 
-python styler.py -t style_tpl.json -v ../dark-matter/dark-matter_vars.json -o ../dark-matter/dark-matter.json
+python styler.py -t style_tpl.json -v dark-matter_vars.json -o dark-matter.json
 
 '''
 
-import json, os, sys, argparse
+import os
+import sys
+import re
+import json
+import argparse
 
-def replace_var(source, string, value):
-  return source.replace(string, value)
-  
+
 def check_file(filename):
-  if not os.path.isfile(filename):
-    print ("Could not found a regular file: "+filename)
-    sys.exit(1)
+    if not os.path.isfile(filename):
+        print('Could not found a regular file: {}'.format(filename))
+        sys.exit(1)
 
-def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-t', dest='templateFile', type=str, help='style template file', required=True)
-  parser.add_argument('-v', dest='templateValuesFile', type=str, help='style template values file', required=True)
-  parser.add_argument('-o', dest='outputFile', type=str, help='style output file', required=True)
-  args = parser.parse_args()
-  
-  vars_json_file = args.templateValuesFile
-  tpl_json_file = args.templateFile
-  style_json_filename = args.outputFile
-  
-  check_file(vars_json_file)
-  check_file(tpl_json_file)
-  
-  style_tpl = open(tpl_json_file, 'r').read()
-  style_json_file = open(style_json_filename, 'w')
-  
-  with open(vars_json_file) as values:
-    d = json.load(values)
-  
-  style_json = style_tpl
-  
-  for k, v in d.items():
-    style_json = replace_var(style_json, '@'+k+'@', v)
-     
-  style_json_file.write(style_json)    
-  style_json_file.close()
-  
-  print "Done!"
+
+def generate_style(tpl_file, vars_json_file, style_file):
+    # Check that files exist
+    check_file(tpl_file)
+    check_file(vars_json_file)
+
+    # Load style variables
+    with open(vars_json_file) as f:
+        d = json.load(f)
+
+    # Load style template
+    with open(tpl_file, 'r') as f:
+        style_json = f.read()
+
+    # Replace variables in the template
+    for var_name, value in d.items():
+        style_json = style_json.replace('@{}@'.format(var_name), value)
+
+    # Check there are not unreplaced
+    for match in re.finditer(r'@.*@', style_json, re.MULTILINE):
+        print('WARNING: Unreplaced variable: {}'.format(match.group()))
+
+    # Write the new style
+    with open(style_file, 'w') as f:
+        f.write(style_json)
+
+    print('Wrote `{}`.'.format(style_file))
+
+
 if __name__ == '__main__':
-  main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-t', '--template', type=str,
+                        help='style template file', required=True)
+
+    parser.add_argument('-v', '--variables', type=str,
+                        help='style variables json file', required=True)
+
+    parser.add_argument('-o', '--output', type=str,
+                        help='style output file', required=True)
+
+    args = parser.parse_args()
+
+    generate_style(args.template, args.variables, args.output)
